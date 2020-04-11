46be9851d089e1d94c38b6f3d80ec7e7f96fd854
David Padilla Montero <dpm00040@red.ujaen.es>
2020-04-11 10:21:10 +0200

    Añadida página web

---
+++ DAMorado/src/main/java/com/morado/damorado/AppConfig.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.morado.damorado;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+
+
+@ApplicationScoped
+@FacesConfig
+public class AppConfig {
+}
---
+++ DAMorado/src/main/java/com/morado/damorado/JAXRSConfiguration.java
@@ -0,0 +1,10 @@
+package com.morado.damorado;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
